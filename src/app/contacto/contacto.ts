4e3cb79d656d57e94dcd8ea9467760770b80d90d
scarlethissgif <scarethliss.agreda01@gmail.com>
2026-08-06 09:48:32 -0500

    modi

--- src/app/contacto/contacto.ts
+++ src/app/contacto/contacto.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl,FormGroup, FormBuilder } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
 export class Contacto {
 
+  formularioContacto;
+
+  constructor(private fb: FormBuilder) {
+    this.formularioContacto = this.fb.group({
+      name: [''],
+      email: [''],
+      telefono: [''],
+      descripcion: ['']
+    });
+  }
+
+  enviar() {
+    console.log(this.formularioContacto.value);
+    alert('Informacion enviada')
+  }
 }
--- src/app/productos/productos.ts
+++ src/app/productos/productos.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './productos.html',
   styleUrl: './productos.css',
 })
+
 export class Productos {
+
+  productos = [
+    { id: 1, nombre: 'laptop', precio: 588, stock: 5, url: "https://neutronpcgamer.com/wp-content/uploads/2026/07/Laptop-Gamer-HP-Victus-15.png"},
+    { id: 2, nombre: 'mouse', precio: 85, stock: 0, url:"https://neutronpcgamer.com/wp-content/uploads/2023/11/Mouse-Logitech-G203-negro.png.webp"},
+    { id: 3, nombre: 'telado', precio: 120, stock: 4, url:"https://neutronpcgamer.com/wp-content/uploads/2023/11/Teclado-Mecanico-Redragon-Fizz-Pro-K616-Rgb-Blanco_Gris.png.webp"},
+    { id: 4, nombre: 'silla gamer', precio: 98, stock: 1, url:"https://neutronpcgamer.com/wp-content/uploads/2026/06/Silla-Gamer-Cougar-Armor-Elite.png"},
+    { id: 5, nombre: 'parlante', precio: 50, stock: 20, url:"https://neutronpcgamer.com/wp-content/uploads/2023/11/Parlante-Logitech-Z313-2.1-Negro.png.webp" },
+    { id: 6, nombre: 'Mousepad', precio: 30, stock: 0, url: "https://neutronpcgamer.com/wp-content/uploads/2023/11/MOUSE-PAD-cougar-ARENA-XL-800-x-300.png.webp"},
+    { id:7, nombre: 'Monitor Gamer Curvo ', precio:250, stock:10, url:'https://neutronpcgamer.com/wp-content/uploads/2026/06/Monitor-Gamer-Curvo-MSI-MAG-242C.png'},
+    { id:8, nombre: 'Controlador Corebox V3', precio:20, stock:0, url:'https://neutronpcgamer.com/wp-content/uploads/2023/12/Controlador-Corebox-V3.png.webp'},
+    { id:9, nombre: 'Computadora Gamer Intel Core ', precio:600, stock:30, url:'https://neutronpcgamer.com/wp-content/uploads/2023/12/case-gamer-rgb-cougar-archon-2.png'},
+    { id:10, nombre: 'Dron dji mini 3 rc con camara 4k hdr', precio:850, stock:0, url:'https://neutronpcgamer.com/wp-content/uploads/2024/07/Dron-dji-mini-3-rc-con-camara-ligera-4k-hdr-2.png.webp'}
+  ];
 
 }
